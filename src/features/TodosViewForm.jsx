989f10bee6331403098dc8215462e07b7872d806
millicent traylor <millicenttraylor7@gmail.com>
2026-01-19 14:05:00 -0600

    week 10 sorting and filtering completed

---
+++ src/features/TodosViewForm.jsx
@@ -0,0 +1,53 @@
+function TodosViewForm({
+  sortField,
+  setSortField,
+  sortDirection,
+  setSortDirection,
+  queryString,
+  setQueryString,
+}) {
+  const preventRefresh = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={preventRefresh}>
+      <div>
+        <label htmlFor="searchTodos">Search todos:</label>
+        <input
+          id="searchTodos"
+          type="text"
+          value={queryString}
+          onChange={(e) => setQueryString(e.target.value)}
+        />
+        <button type="button" onClick={() => setQueryString('')}>
+          Clear
+        </button>
+      </div>
+
+      <div>
+        <label htmlFor="sortField">Sort by</label>
+        <select
+          id="sortField"
+          onChange={(e) => setSortField(e.target.value)}
+          value={sortField}
+        >
+          <option value="title">Title</option>
+          <option value="createdTime">Time added</option>
+        </select>
+
+        <label htmlFor="sortDirection">Direction</label>
+        <select
+          id="sortDirection"
+          value={sortDirection}
+          onChange={(e) => setSortDirection(e.target.value)}
+        >
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </div>
+    </form>
+  );
+}
+
+export default TodosViewForm;
